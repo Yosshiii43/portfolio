3e4d8dc1fcba8722a24ba4fb26236fa52f60f48a
Yosshiii43 <daisy440440@gmail.com>
2024-10-15 00:41:34 +0900

    header/mainVisualまで

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//ハンバーガーメニュー
+document.querySelector( '.js-hamburger' ).addEventListener(
+	'click',
+	() => {
+		document.querySelector( '.js-hamburger' ).classList.toggle( 'is-open' );
+		document.querySelector( '.p-header__menu' ).classList.toggle( 'is-open' );
+		document.querySelector( 'body' ).classList.toggle( 'is-open' );
+	}
+);
